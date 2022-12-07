4043af726157f00a7b6a533431bc7c52a83276b6
ziansin <pilchza@ssfs.org>
2022-12-07 10:25:46 -0500

    swap and rotate left

--- intro_lists.py
+++ intro_lists.py
@@ -4,8 +4,20 @@ def swap(list_one):
     :param list_one: a list of at least two elements
     :return: the same list with the first and last elements swapped
     """
-    pass # make sure to remove this line before beginning work on this function
+    num = len(list_one)
+    if num > 2:
+        list_one = list_one[-1], list_one[1:(num - 1)], list_one[0]
+    else:
+        list_one = list_one[-1], list_one[0]
+    list_one = str(list_one)
+    list_one = list_one.replace("[", "")
+    list_one = list_one.replace("]", "")
+    list_one = list_one.replace("(", "[")
+    list_one = list_one.replace(")", "]")
+    return list_one
 
+list1 = [1, 2, 3, 4]
+print(swap(list1))
 
 def rotate_left(list_one):
     """
@@ -13,8 +25,17 @@ def rotate_left(list_one):
     :param list_one: A list consisting of exactly three integers
     :return: a list where all the elements have been shifted 1 place to the left
     """
-    pass # make sure to remove this line before beginning work on this function
+    num = len(list_one)
+    list_one = list_one[1:(num - 1)], list_one[-1], list_one[0]
+    list_one = str(list_one)
+    list_one = list_one.replace("[", "")
+    list_one = list_one.replace("]", "")
+    list_one = list_one.replace("(", "[")
+    list_one = list_one.replace(")", "]")
+    return list_one
 
+list2 = [1, 2, 3, 4]
+print(rotate_left(list2))
 
 def max_end(list_one):
     """
